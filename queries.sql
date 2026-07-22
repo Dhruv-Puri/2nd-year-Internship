4bd0ceb84aa4958f8bfa1cf20a1d96300a68526d
Dhruv-Puri <puridhruv2005@gmail.com>
2026-07-22 15:07:24 +0530

    Refresh

---
+++ queries.sql
@@ -0,0 +1,17 @@
+-- DROP SCHEMA public CASCADE;
+-- CREATE SCHEMA public;
+
+
+SELECT * FROM announcements;
+SELECT * FROM attendance;
+SELECT * FROM clubs;
+SELECT * FROM events;
+SELECT * FROM notifications;
+SELECT * FROM rsvps;
+SELECT * FROM user_clubs;
+SELECT * FROM users;
+SELECT * FROM otps;
+SELECT * FROM email_quotas;
+
+
+
